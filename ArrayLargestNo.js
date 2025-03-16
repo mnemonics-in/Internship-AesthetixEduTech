0b641b8c0131dfb8fbb67c3c27ace868de2afc28
thomasmathew <thomasmathew1404@gmail.com>
2025-03-16 10:57:06 +0530

    Array Sorted

---
+++ ArrayLargestNo.js
@@ -0,0 +1,25 @@
+//Find largest number of array using function
+
+const arrVal=[100,500,50,10,400,300,1000,20,700,450,];
+console.log("Array Values are",arrVal);
+
+function funSort(arr1){
+
+    for(let i=0;i<arr1.length-1;i++){
+        for(let j=i+1; j<arr1.length;j++){
+
+            var temp;
+            if(arr1[i]>arr1[j]){
+                temp=arr1[i];
+                arr1[i]=arr1[j];
+                arr1[j]=temp;
+            }
+        }
+    }
+
+    return arr1
+}
+
+const SorArray=funSort(arrVal);
+
+console.log("After Sorting Array ",SorArray);
